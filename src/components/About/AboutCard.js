434f453be0deb0def6e50198721e1ea18e9700b1
dheerajmehta <86247811+dheerajmehtaa@users.noreply.github.com>
2022-01-06 00:18:02 +0530

    a

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -31,7 +31,7 @@ function AboutCard() {
           <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Dheeraj</footer>
         </blockquote>
       </Card.Body>
     </Card>
