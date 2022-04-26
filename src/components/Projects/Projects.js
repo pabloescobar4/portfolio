40db7cbdcfb48871797456655b5eba0304b01a13
dheerajmehta <dheeraj_fw11_310@masai.school>
2022-04-26 17:27:01 +0530

    added pepperfry

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,7 @@ import Particle from "../Particle";
 
 import leaf from "../../Assets/Projects/leaf.png";
 import spot from "../../Assets/Projects/spot.png";
+import pepper from "../../Assets/Projects/pepper.png";
 
 import chatify from "../../Assets/Projects/chatify.png";
 
@@ -53,11 +54,26 @@ function Projects() {
               imgPath={spot}
               isBlog={false}
               title="Spotify"
-              description="Using HTML, CSS, Node.js, Express & MongoDB, I have built a clone of MPL.live. Which allows users to win rewards and cash prizes by playing the games of their choice."
+              description="Using HTML, CSS, Node.js, Express & MongoDB, I have built a clone of Spotify. Which allows users to listen millions of songs and podcasts of your choice."
              
             
               link="https://github.com/pabloescobar4/spotifyyyy"
               link1="https://spotifydk.herokuapp.com/"
+          
+              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard1
+              imgPath={pepper}
+              isBlog={false}
+              title="Pepperfry"
+              description="Using React js,Redux, Tailwind CSS, JSON, Material UI and  I have built a clone of Pepperfry. Which allows users to shop any type of furniture in India."
+             
+            
+              link="https://github.com/pabloescobar4/pepperfry"
+              link1="https://pepperfry-git-main-pabloescobar4.vercel.app/"
           
               
             />
