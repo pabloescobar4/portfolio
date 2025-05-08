8879323885fe75b67fa67f88f19b6e2921f2612e
dheerajmehta <dheeraj_fw11_310@masai.school>
2025-05-09 01:31:06 +0530

    dfa

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,5 +1,5 @@
-import React from "react";
-import GitHubCalendar from "react-github-calendar";
+// import React from "react";
+// import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
 
 function Github() {
@@ -14,16 +14,16 @@ function Github() {
   };
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
+      {/* <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
         Days I <strong className="purple">Code</strong>
-      </h1>
-      <GitHubCalendar
+      </h1> */}
+      {/* <GitHubCalendar
         username="pabloescobar4"
         blockSize={15}
         blockMargin={5}
         theme={colourTheme}
         fontSize={16}
-      />
+      /> */}
     </Row>
   );
 }
